a18519e91d5a593657eda66c87406517d94c1cc0
Derek Cannon <derektcannon@gmail.com>
2021-10-23 02:03:07 -0700

    Monkey-patch fix for failing specs on Rails 4.2

--- spec/rails_spec_helper.rb
+++ spec/rails_spec_helper.rb
@@ -68,3 +68,19 @@ class ApplicationController < ActionController::Base
 end
 
 require 'rspec/rails'
+
+# https://github.com/rails/rails/issues/34790#issuecomment-450502805
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
